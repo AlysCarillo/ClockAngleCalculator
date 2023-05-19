aa3387e90f43eb58bfebdf7b7c20ef86d47173ef
Alys Anthea Carillo <alysanthea.carillo@cit.edu>
2023-05-19 09:32:50 +0800

    Create IClock interface

---
+++ ClockAngleCalculator/Clock/ICLock.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockAngleCalculator.Clock
+{
+    public interface ICLock
+    {
+        int CalculateAngle();
+    }
+}
