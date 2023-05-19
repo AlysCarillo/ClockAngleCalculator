dd0c8c7fe37b1af120fb1f23f9a81e6d2c342a7c
Alys Anthea Carillo <alysanthea.carillo@cit.edu>
2023-05-19 10:04:30 +0800

    Implement CalculateAngle method

---
+++ ClockAngleCalculator/Clock/Clock.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClockAngleCalculator.Clocks
+{
+    public class Clock : ICLock
+    {
+        public int Hours { get; }
+        public int Minutes { get; }
+
+        public Clock(int hours, int minutes)
+        {
+            if (hours < 0 || hours > 12 || minutes < 0 || minutes >= 60)
+            {
+                throw new ArgumentException("Invalid hours or minutes.");
+            }
+
+            Hours = hours;
+            Minutes = minutes;
+        }
+
+        public int CalculateAngle()
+        {
+            // 1 hour = 30 degrees (360 / 12)
+            double hourAngle = 0.5 * (Hours * 60 + Minutes);
+
+            // 1 minute = 6 degrees (360 / 60)
+            double minuteAngle = 6 * Minutes;
+
+            // Find the difference between two angles
+            int diff = (int)Math.Abs(hourAngle - minuteAngle);
+            int angle = Math.Min(360 - diff, diff);
+
+            return angle;
+        }
+    }
+}
--- ClockAngleCalculator/Clock/ICLock.cs
+++ ClockAngleCalculator/Clock/ICLock.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClockAngleCalculator.Clock
+namespace ClockAngleCalculator.Clocks
 {
     public interface ICLock
     {
