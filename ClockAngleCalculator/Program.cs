a9d68a52ac7592430cdab33fa11d79fa0614106c
Alys Anthea Carillo <alysanthea.carillo@cit.edu>
2023-05-19 10:04:39 +0800

    Create the main program

--- ClockAngleCalculator/Program.cs
+++ ClockAngleCalculator/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ClockAngleCalculator.Clocks;
+
+namespace ClockAngleCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Clock Angle Calculator!\n");
+
+            Console.Write("Please enter hours (12-hours format): ");
+            var hrs = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Please enter minutes: ");
+            var min = Convert.ToInt32(Console.ReadLine());
+
+            ICLock clock = new Clock(hrs, min);
+
+            Console.WriteLine($"\nThe lesser angle in degrees between hours arrow and minutes arrow is {clock.CalculateAngle()}. \n");
+        }
+    }
+}
